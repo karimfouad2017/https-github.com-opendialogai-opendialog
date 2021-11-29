c2dd746d52735f7df7cd52dac4281d0ce5da14fb
patshone-gsl <65708894+patshone-gsl@users.noreply.github.com>
2021-11-29 20:33:05 +0000

    adds Request class back in to UsersController

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserCollection;
 use App\Http\Resources\UserResource;
 use App\User;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
