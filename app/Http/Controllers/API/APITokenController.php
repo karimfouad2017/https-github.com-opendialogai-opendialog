aa9d71bddb3a07ebc73a73e9a5ddee84e26e3af5
Goran Ambardjiev <goranam@worthavegroup.com>
2022-01-24 10:37:05 +0100

    Removes user_id payload from the request

--- app/Http/Controllers/API/APITokenController.php
+++ app/Http/Controllers/API/APITokenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RefreshAPITokenRequest;
 use App\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
@@ -13,19 +14,11 @@ class APITokenController extends Controller
     /**
      * Refreshes the user token
      *
-     * @param RefreshAPITokenRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function refreshToken(RefreshAPITokenRequest $request): \Illuminate\Http\Response
+    public function refreshToken(): \Illuminate\Http\Response
     {
-        $user = User::where('id', $request->user_id)->first();
-        if (!$user) {
-            return response('Invalid user id provided', 422);
-        }
-        if ($user->api_token != $request->bearerToken()) {
-            return response('Invalid token provided', 422);
-        }
-
+        $user = Auth::user();
         $user->api_token = Str::random(60);
         try {
             $user->update();
--- tests/Feature/RefreshAPITokenTest.php
+++ tests/Feature/RefreshAPITokenTest.php
@@ -19,10 +19,7 @@ class RefreshAPITokenTest extends TestCase
     public function testRefreshTokenSuccess()
     {
         $response = $this->actingAs($this->user, 'api')
-            ->json('POST', '/admin/api/refresh-token',
-                [
-                    'user_id' => $this->user->id
-                ],
+            ->json('POST', '/admin/api/refresh-token', [],
                 [
                     'Authorization' => 'Bearer ' . $this->user->api_token
                 ]);
@@ -30,41 +27,13 @@ class RefreshAPITokenTest extends TestCase
         $response->assertStatus(200);
 
         $newToken = $response->json('api_token');
-        $this->assertNotEquals($newToken, $this->user->api_token);
-
-        $this->user->refresh();
         $this->assertEquals($newToken, $this->user->api_token);
 
         // Retry with the new generated token
         $this->actingAs($this->user, 'api')
-            ->json('POST', '/admin/api/refresh-token',
-                [
-                    'user_id' => $this->user->id
-                ],
+            ->json('POST', '/admin/api/refresh-token', [],
                 [
                     'Authorization' => 'Bearer ' . $this->user->api_token
                 ])->assertStatus(200);
-    }
-
-    public function testRefreshTokenFailInvalidUserId()
-    {
-        $this->actingAs($this->user, 'api')
-            ->json('POST', '/admin/api/refresh-token', [
-                'user_id' => 'Some Id'
-            ], [
-                'Authorization' => 'Bearer ' . $this->user->api_token
-            ])->assertStatus(422);
-    }
-
-    public function testRefreshTokenFailInvalidToken()
-    {
-        $this->actingAs($this->user, 'api')
-            ->json('POST', '/admin/api/refresh-token',
-                [
-                    'user_id' => $this->user->id
-                ],
-                [
-                    'Authorization' => 'Bearer ' . 'Some token'
-                ])->assertStatus(422);
     }
 }
