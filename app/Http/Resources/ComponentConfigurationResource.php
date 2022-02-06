e01cb9fbdb806be501a46e75c072aa96e78c3fd9
Aniekan Offiong <ekanoffiong@gmail.com>
2022-02-06 01:49:13 +0100

    Added changes to filter out hidden fields for configurations as defined inside the configuration class

--- app/Http/Resources/ComponentConfigurationResource.php
+++ app/Http/Resources/ComponentConfigurationResource.php
@@ -3,6 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use OpenDialogAi\ActionEngine\Service\ActionComponentServiceInterface;
+use OpenDialogAi\InterpreterEngine\Service\InterpreterComponentServiceInterface;
+use OpenDialogAi\PlatformEngine\Services\PlatformComponentServiceInterface;
 
 class ComponentConfigurationResource extends JsonResource
 {
@@ -14,6 +17,48 @@ class ComponentConfigurationResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $componentId = $this->component_id;
+        $type = ComponentHelper::parseComponentId($componentId);
+        
+        $component = null;
+
+        switch ($type) {
+            case 'platform':
+                $component = resolve(PlatformComponentServiceInterface::class)->get($componentId);
+                break;
+            case 'interpreter':
+                $component = resolve(InterpreterComponentServiceInterface::class)->get($componentId);
+                break;
+            case 'action':
+                $component = resolve(ActionComponentServiceInterface::class)->get($componentId);
+                break;
+        }
+
+        /** @var array $hiddenFields */
+        $hiddenFields = $component::$configurationClass::getHiddenFields();
+        
+        $originalArray = parent::toArray($request);
+
+        $finalArray = $this->filterHiddenFields($originalArray, $hiddenFields);
+
+        return $finalArray;
+    }
+
+    protected function filterHiddenFields($originalArray, $hiddenFields)
+    {
+        $finalArray = [];
+
+        foreach ($originalArray as $key => $value) {
+            if (!in_array($key, $hiddenFields)) {
+                if (is_array($value)) {
+                    $finalArray[$key] = $this->filterHiddenFields($value, $hiddenFields);
+                    continue;
+                }
+
+                $finalArray[$key] = $value;
+            }
+        }
+
+        return $finalArray;
     }
 }
